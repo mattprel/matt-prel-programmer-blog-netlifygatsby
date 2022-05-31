62255fb4879ab25c0adc80c71ac43c966cea3889
Matt Prel <85019814+mattprel@users.noreply.github.com>
2022-05-31 13:11:10 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Matt Prel's Programmer Blog",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Join me on my journey through learning to program and bulid exciting and unique projects.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
